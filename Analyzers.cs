fcbe47e8ae5a2aa268e31a60eb40803735086c6b
Ismael Avila <ismaelg.avilag@gmail.com>
2023-09-16 23:21:30 -0600

    Analizador léxico
    - Se creó la clase Analizers, la cual contiene al analizador léxico y en un futuro, al analizador sintáctico.
    - Se creó la estructura básica del analizador léxico.

---
+++ Analyzers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler
+{
+    internal class Analyzers
+    {
+
+        public static Element lexicalAnalyze(string s)
+        {
+            int state = 0;
+            string lexeme = "", token = "error";
+
+            for(int i=0; i<s.Length; i++) {
+                switch(state) {
+                    case 0:
+                    break;
+
+                    case 1: break;
+
+                    case 2: break;
+
+                    case 3: break;
+
+                    case 4: break;
+
+                    case 5: break;
+
+                    case 6: break;
+
+                    case 7: break;
+
+                    case 8: break;
+
+                    case 9: break;
+
+                    case 10: break;
+
+                    case 11: break;
+
+                    case 12: break;
+
+                    case 13: break;
+
+                    case 14: break;
+
+                    case 15: break;
+
+                    case 16: break;
+
+                    case 17: break;
+
+                    case 18: break;
+
+                    case 19: break;
+
+                    case 21: break;
+
+                    case 22: break;
+
+                    case 23: break;
+
+                    case 24: break;
+
+                    case 25: break;
+
+                    case 26: break;
+
+                }
+            }
+
+
+        }
+
+    }
+}
