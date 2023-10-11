19daabc3617d782d8bed7b218741493f6ae261a5
Ismael Avila <ismaelg.avilag@gmail.com>
2023-10-11 00:26:39 -0600

    Modificaciones en la interfaz gráfica

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,16 +37,17 @@
             columnHeader1 = new ColumnHeader();
             columnHeader2 = new ColumnHeader();
             columnHeader3 = new ColumnHeader();
+            buttonSintaxAnalyzer = new Button();
             SuspendLayout();
             // 
             // buttonLexicalAnalyzer
             // 
-            buttonLexicalAnalyzer.BackColor = Color.FromArgb(103, 137, 131);
+            buttonLexicalAnalyzer.BackColor = Color.FromArgb(6, 214, 160);
             buttonLexicalAnalyzer.Cursor = Cursors.Hand;
             buttonLexicalAnalyzer.FlatStyle = FlatStyle.Popup;
             buttonLexicalAnalyzer.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            buttonLexicalAnalyzer.ForeColor = Color.White;
-            buttonLexicalAnalyzer.Location = new Point(579, 206);
+            buttonLexicalAnalyzer.ForeColor = Color.Black;
+            buttonLexicalAnalyzer.Location = new Point(222, 315);
             buttonLexicalAnalyzer.Name = "buttonLexicalAnalyzer";
             buttonLexicalAnalyzer.Size = new Size(180, 70);
             buttonLexicalAnalyzer.TabIndex = 0;
@@ -59,11 +60,11 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(540, 9);
+            label1.Location = new Point(268, 9);
             label1.Name = "label1";
-            label1.Size = new Size(253, 60);
+            label1.Size = new Size(823, 60);
             label1.TabIndex = 1;
-            label1.Text = "Compilador";
+            label1.Text = "Compilador - Seminario de Traductores 2";
             // 
             // textBoxInput
             // 
@@ -71,12 +72,12 @@
             textBoxInput.BorderStyle = BorderStyle.None;
             textBoxInput.Font = new Font("Fira Code", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
             textBoxInput.ForeColor = Color.FromArgb(97, 175, 239);
-            textBoxInput.Location = new Point(12, 57);
+            textBoxInput.Location = new Point(14, 95);
             textBoxInput.Margin = new Padding(5);
             textBoxInput.Multiline = true;
             textBoxInput.Name = "textBoxInput";
             textBoxInput.PlaceholderText = "Ingresa el código a analizar:";
-            textBoxInput.Size = new Size(350, 350);
+            textBoxInput.Size = new Size(1254, 173);
             textBoxInput.TabIndex = 2;
             // 
             // textBoxOutput
@@ -87,13 +88,13 @@
             textBoxOutput.Enabled = false;
             textBoxOutput.Font = new Font("Fira Code", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
             textBoxOutput.ForeColor = Color.Red;
-            textBoxOutput.Location = new Point(920, 57);
+            textBoxOutput.Location = new Point(664, 425);
             textBoxOutput.Multiline = true;
             textBoxOutput.Name = "textBoxOutput";
             textBoxOutput.ReadOnly = true;
-            textBoxOutput.Size = new Size(350, 350);
+            textBoxOutput.Size = new Size(606, 250);
             textBoxOutput.TabIndex = 3;
-            textBoxOutput.Text = "Análisis Sintáctico - No disponible por el momento";
+            textBoxOutput.Text = "Análisis Sintáctico no disponible... por el momento";
             // 
             // listView1
             // 
@@ -101,9 +102,9 @@
             listView1.GridLines = true;
             listView1.HeaderStyle = ColumnHeaderStyle.Nonclickable;
             listView1.HoverSelection = true;
-            listView1.Location = new Point(11, 458);
+            listView1.Location = new Point(12, 425);
             listView1.Name = "listView1";
-            listView1.Size = new Size(1260, 383);
+            listView1.Size = new Size(600, 250);
             listView1.TabIndex = 4;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
@@ -111,26 +112,41 @@
             // columnHeader1
             // 
             columnHeader1.Text = "Lexema";
-            columnHeader1.Width = 416;
+            columnHeader1.Width = 195;
             // 
             // columnHeader2
             // 
             columnHeader2.Text = "Token";
             columnHeader2.TextAlign = HorizontalAlignment.Center;
-            columnHeader2.Width = 416;
+            columnHeader2.Width = 195;
             // 
             // columnHeader3
             // 
             columnHeader3.Text = "Tipo";
             columnHeader3.TextAlign = HorizontalAlignment.Center;
-            columnHeader3.Width = 416;
+            columnHeader3.Width = 195;
+            // 
+            // buttonSintaxAnalyzer
+            // 
+            buttonSintaxAnalyzer.BackColor = Color.FromArgb(255, 209, 102);
+            buttonSintaxAnalyzer.Cursor = Cursors.Hand;
+            buttonSintaxAnalyzer.FlatStyle = FlatStyle.Popup;
+            buttonSintaxAnalyzer.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonSintaxAnalyzer.ForeColor = Color.Black;
+            buttonSintaxAnalyzer.Location = new Point(877, 315);
+            buttonSintaxAnalyzer.Name = "buttonSintaxAnalyzer";
+            buttonSintaxAnalyzer.Size = new Size(180, 70);
+            buttonSintaxAnalyzer.TabIndex = 6;
+            buttonSintaxAnalyzer.Text = "Analizador Sintáctico";
+            buttonSintaxAnalyzer.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(24, 29, 49);
-            ClientSize = new Size(1282, 853);
+            BackColor = Color.FromArgb(7, 59, 76);
+            ClientSize = new Size(1282, 687);
+            Controls.Add(buttonSintaxAnalyzer);
             Controls.Add(listView1);
             Controls.Add(textBoxOutput);
             Controls.Add(textBoxInput);
@@ -139,7 +155,7 @@
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
+            Text = "Compilador - Seminario de Traductores 2";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -154,5 +170,6 @@
         private ColumnHeader columnHeader1;
         private ColumnHeader columnHeader2;
         private ColumnHeader columnHeader3;
+        private Button buttonSintaxAnalyzer;
     }
 }
